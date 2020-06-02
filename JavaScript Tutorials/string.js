94c108444654cba14fa6fa3d2bd32992dab0e5d5
Sahil Mishra <sahilmishra0012@outlook.com>
2020-06-02 13:39:23 +0530

    String Indexing

---
+++ JavaScript Tutorials/string.js
@@ -0,0 +1,9 @@
+var name = "Sahil Mishra"
+
+console.log(name.substr(2,7)) // Start to Length
+console.log(name.substring(2,7)) // Start Index to End Index
+console.log(name.slice(2,7)) // Start Index to End Index
+
+console.log(name.substr(-5,3)) // Start to Length
+console.log(name.substring(-5,-3)) // Start Index to End Index
+console.log(name.slice(-5,-3)) // Start Index to End Index
