53969a81dabc390ac4c7010954df6f3995a92fa3
Sahil Mishra <sahilmishra0012@outlook.com>
2020-06-02 13:39:30 +0530

    Array Indexing

---
+++ JavaScript Tutorials/array.js
@@ -0,0 +1,5 @@
+var name1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+var name2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+
+console.log(name1.splice(2,5)) // Start Index to End Index. It updates the array.
+console.log(name2.slice(2,5)) // Start Index to End Index
