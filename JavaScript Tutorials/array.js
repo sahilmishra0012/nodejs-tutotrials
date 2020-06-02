bc49d02ec48b59880565a5cc846c8cebfc5c7a31
Sahil Mishra <sahilmishra0012@outlook.com>
2020-06-02 18:51:55 +0530

    General Array Operations

--- JavaScript Tutorials/array.js
+++ JavaScript Tutorials/array.js
@@ -1,5 +1,31 @@
-var name1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
-var name2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+let arr = [1,2,3,4]
 
-console.log(name1.splice(2,5)) // Start Index to End Index. It updates the array.
-console.log(name2.slice(2,5)) // Start Index to End Index
+console.log(arr)
+console.log(arr.length)
+console.log(arr[2])
+console.log("=============================================")
+for(let i=0;i<arr.length;i++)
+{
+    console.log(arr[i])
+}
+console.log("=============================================")
+for(let i of arr){
+    console.log(i)
+}
+console.log("=============================================")
+for(let index in arr){
+    console.log(index)
+}
+console.log("=============================================")
+console.log("=============================================")
+let fruits = ["Apple", "Mango", "Banana"]
+console.log(fruits)
+fruits.push("Cherry","Lemon")// Insert at end
+console.log(fruits)
+fruits.pop()// Delete from end
+fruits.pop()
+console.log(fruits)
+fruits.unshift("Cherry","Lemon")// Insert at beginning
+console.log(fruits)
+fruits.shift()// Delete from beginning
+console.log(fruits)
